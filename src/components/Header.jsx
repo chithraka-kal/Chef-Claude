3aa40ab18a07326821260f7462aed1f50b783126
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-01 10:40:41 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Chef Claude</h1>
+      <p className="header__subtitle">Your personal recipe assistant</p>
+    </header>
+  );
+}
+
+export default Header;
