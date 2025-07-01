28234c21a25fdbdc184419c7b4e77385d28004fc
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-02 02:43:37 +0530

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,23 @@
+import React from 'react';
 function Main() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientItems = ingredients.map(
+    (ingredient, index) => <li key={index}>{ingredient}</li>
+    )
+
+    function handelSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const newIngredient = formData.get('ingredient')
+        setIngredients([...ingredients, newIngredient])
+        event.target.reset()
+    }
+
   return (
     <main>
-      <form className="ingredient-form">
+      <form onSubmit={handelSubmit} className="ingredient-form">
         <input 
             type="text"
             placeholder="e.g. oregano"
@@ -10,6 +26,9 @@ function Main() {
         />
         <button type="submit">Add Ingredient</button>
       </form>
+      <ul>
+        {ingredientItems}
+      </ul>
     </main>
   );
 }
