982fdb9548dd0994fda62bd0e733dcb154997ea4
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-05 13:30:11 +0530

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,50 +1,63 @@
-import React from 'react';
-import ClaudeRecipe from './ClaudeRecipe';
-import IngredientsList from './IngredientsList';
-import { getRecipeFromMistral } from "../ai";
+import React from "react"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromGoogle } from "../ai"
+
 function Main() {
+  const [ingredients, setIngredients] = React.useState([
+    "all the main spices",
+  ])
 
-    const [ingredients, setIngredients] = React.useState(["all the main spices", "potato", "flour", "coconut oil", "onion", "garlic", "salt", "pepper",])
+  const [recipe, setRecipe] = React.useState("")
+  const [recipeShown, setRecipeShown] = React.useState(false)
 
-    const ingredientItems = ingredients.map(
-    (ingredient, index) => <li key={index}>{ingredient}</li>
-    )
+  const ingredientItems = ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ))
 
-    const [recipeShown, setRecipeShown] = React.useState(false);
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromGoogle(ingredients)
+    setRecipe(recipeMarkdown)
+    setRecipeShown(true)
+  }
 
-    async function getRecipe() {
-        const recipeMarkdown = await getRecipeFromMistral(ingredients)
-        console.log(recipeMarkdown)
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient")
+    if (newIngredient !== "") {
+      setIngredients([...ingredients, newIngredient])
     }
-
-
-    function addIngredient(formData) {
-        const newIngredient = formData.get('ingredient')
-        if(newIngredient !== ''){setIngredients([...ingredients, newIngredient])}
-    }
-    
-    
+  }
 
   return (
     <main>
-      <form action={addIngredient} className="ingredient-form">
-        <input 
-            type="text"
-            placeholder="e.g. oregano"
-            aria-label="Add Ingredient"
-            name="ingredient"
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          addIngredient(new FormData(e.target))
+          e.target.reset()
+        }}
+        className="ingredient-form"
+      >
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add Ingredient"
+          name="ingredient"
         />
         <button type="submit">Add Ingredient</button>
       </form>
-            {ingredients.length > 0 && 
-            <IngredientsList 
-              ingredients={ingredients} 
-              ingredientItems={ingredientItems} 
-              getRecipe={getRecipe}/>
-            }
-      {recipeShown && <ClaudeRecipe />}
+
+      {ingredients.length > 0 && (
+        <IngredientsList
+          ingredients={ingredients}
+          ingredientItems={ingredientItems}
+          getRecipe={getRecipe}
+        />
+      )}
+
+      {recipeShown && <ClaudeRecipe recipe={recipe} />}
     </main>
-  );
+  )
 }
 
-export default Main;
+export default Main
