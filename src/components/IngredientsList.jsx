62d6c10809dd8d6c1c22280e7d4409689005afd4
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-07-05 13:29:59 +0530

    ingerdient list

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,16 +1,20 @@
-export default function IngredientsList(props){
-    return(
-    <section className='ingredients-section'>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{props.ingredientItems}</ul>
-                {props.ingredients.length > 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={props.getRecipe}>Get a recipe</button>
-                </div>}
-            </section>
+export default function IngredientsList(props) {
+  return (
+    <section className="ingredients-section">
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {props.ingredientItems}
+      </ul>
 
-    )
+      {props.ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.getRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  )
 }
